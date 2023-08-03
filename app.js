3e617bc9d763e141d275b1f9837f57cc2cf09b54
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-08-02 23:11:23 -0600

    87: Request y Response

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-    res.write('Hola mundo');
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
+    res.writeHead(200, {'Content-Type': 'application/csv'});
+    res.write('id, nombre\n');
+    res.write('1, samuel\n');
+    res.write('2, stephanie\n');
+    res.write('3, mireya\n');
     res.end();
 }).listen(8080);
 
