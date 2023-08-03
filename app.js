7f54e43f39f70d9836210d2ec825eae939f78f5d
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-08-02 23:29:35 -0600

    88: Introduccion a express

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 8080;
 
-http.createServer((req, res) => {
-    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
-    res.writeHead(200, {'Content-Type': 'application/csv'});
-    res.write('id, nombre\n');
-    res.write('1, samuel\n');
-    res.write('2, stephanie\n');
-    res.write('3, mireya\n');
-    res.end();
-}).listen(8080);
+app.get('/', (req, res) => {
+    res.send('Hello world')
+});
 
-console.log('Escuchando en el puerto 8080');
+app.get('/hola-mundo', (req, res) => {
+    res.send('Hola mundo en su respectiva ruta')
+});
+
+app.get('*', (req, res) => {
+    res.send('404 | Page not found');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
