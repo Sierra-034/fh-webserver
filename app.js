f545d34ea87d51f69e3bb19e158db53e9b268141
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-08-03 00:25:25 -0600

    90: Servir un sitio wen completo

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const port = 8080;
 // Servir contenido estático
 app.use(express.static('public'))
 
-app.get('/hola-mundo', (req, res) => {
-    res.send('Hola mundo en su respectiva ruta');
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html');
 });
 
 app.get('*', (req, res) => {
