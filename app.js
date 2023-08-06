0f3e7870feeaab689309d2291b821b445ba89d26
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-08-05 20:49:20 -0600

    92: Argumetos desde el controlador

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {
+        nombre: 'Samuel Gómez',
+        titulo: 'Curso de node',
+    });
 });
 
 app.get('/generic', (req, res) => {
